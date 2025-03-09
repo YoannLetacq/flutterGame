3950e69eca3199c84e584778889918107cf4ab36
Yoann <yoann.letacq0@gmail.com>
2025-03-09 20:44:58 +0100

    UPDATE game_repository.dart (createGame use doc(id).set(gameData);UPDATE game_repository_test (check new behaviour)

--- test/game_repository_test.dart
+++ test/game_repository_test.dart
@@ -1,92 +1,89 @@
 import 'package:flutter_test/flutter_test.dart';
-// On importe le paquet "fake_cloud_firestore"
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
-
 import 'package:untitled/repositories/game_repository_interface.dart';
 import 'package:untitled/repositories/game_repository.dart';
 
 void main() {
-  late IGameRepository gameRepository;
   late FakeFirebaseFirestore fakeFirestore;
+  late IGameRepository gameRepository;
 
   setUp(() {
-    // On instancie un Firestore simulé (fake) avant chaque test.
+    // Instanciation du Firestore simulé
     fakeFirestore = FakeFirebaseFirestore();
-
-    // On injecte l'instance mockée dans GameRepository
+    // Injection de l'instance mockée dans le repository
     gameRepository = GameRepository(firestore: fakeFirestore);
   });
 
   group('GameRepository Tests', () {
     test('createGame', () async {
+      const gameId = 'testGameId';
       final sampleGameData = {
+        'id': gameId,
         'cards': ['card1', 'card2'],
         'mode': 'CLASSIQUE',
         'players': {}
       };
 
-      // On appelle la méthode createGame
       await gameRepository.createGame(sampleGameData);
 
-      // Vérifions que le document a bien été créé dans la collection 'games'
-      final gamesCollection = fakeFirestore.collection('games');
-      final querySnapshot = await gamesCollection.get();
-      final docs = querySnapshot.docs;
-      expect(docs.length, 1, reason: 'Un seul document doit être créé.');
-
-      final createdData = docs.first.data();
+      // Vérifier que le document a été créé avec l'ID fourni.
+      final docSnapshot = await fakeFirestore.collection('games').doc(gameId).get();
+      expect(docSnapshot.exists, isTrue);
+      final createdData = docSnapshot.data()!;
+      expect(createdData['id'], equals(gameId));
       expect(createdData['cards'], equals(['card1', 'card2']));
       expect(createdData['mode'], equals('CLASSIQUE'));
       expect(createdData['players'], equals({}));
     });
 
     test('getGame', () async {
-      // Créons d'abord un document Firestore dans le fake
-      final docRef = fakeFirestore.collection('games').doc('myGameId');
-      await docRef.set({
+      final gameId = 'myGameId';
+      await fakeFirestore.collection('games').doc(gameId).set({
+        'id': gameId,
         'cards': ['cardA', 'cardB'],
         'mode': 'RAPIDE',
         'players': {}
       });
 
-      // On appelle la méthode getGame du repository
-      final result = await gameRepository.getGame('myGameId');
+      final result = await gameRepository.getGame(gameId);
       expect(result, isNotNull);
-      expect(result!['cards'], equals(['cardA', 'cardB']));
+      expect(result!['id'], equals(gameId));
+      expect(result['cards'], equals(['cardA', 'cardB']));
       expect(result['mode'], equals('RAPIDE'));
     });
 
     test('updateGame', () async {
-      final docRef = fakeFirestore.collection('games').doc('updateTest');
-      await docRef.set({
+      final gameId = 'updateTest';
+      await fakeFirestore.collection('games').doc(gameId).set({
+        'id': gameId,
         'cards': ['card1'],
         'mode': 'CLASSIQUE',
         'players': {}
       });
 
-      await gameRepository.updateGame('updateTest', {
+      await gameRepository.updateGame(gameId, {
         'cards': ['card1', 'card2', 'card3']
       });
 
-      final updatedSnapshot = await docRef.get();
+      final updatedSnapshot = await fakeFirestore.collection('games').doc(gameId).get();
       final updatedData = updatedSnapshot.data()!;
       expect(updatedData['cards'], equals(['card1', 'card2', 'card3']));
       expect(updatedData['mode'], equals('CLASSIQUE'));
     });
 
     test('deleteGame', () async {
-      final docRef = fakeFirestore.collection('games').doc('deleteTest');
-      await docRef.set({
+      const gameId = 'deleteTest';
+      await fakeFirestore.collection('games').doc(gameId).set({
+        'id': gameId,
         'cards': ['cardX'],
         'mode': 'MODE_X',
         'players': {}
       });
 
-      await gameRepository.deleteGame('deleteTest');
+      await gameRepository.deleteGame(gameId);
 
-      final deletedSnapshot = await docRef.get();
-      expect(deletedSnapshot.exists, isFalse,
-          reason: 'Le document doit être supprimé.');
+      final deletedSnapshot = await fakeFirestore.collection('games').doc(gameId).get();
+      expect(deletedSnapshot.exists, isFalse, reason: 'Le document doit être supprimé.');
     });
   });
 }
