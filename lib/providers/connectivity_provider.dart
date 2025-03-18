0506d7e62d4b99541e25cc1778b2f4a396b9f762
Yoann <yoann.letacq0@gmail.com>
2025-03-18 13:52:35 +0100

    CREATE connectivity_provider.dart (provide the connectivity result to the app)

---
+++ lib/providers/connectivity_provider.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/foundation.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:untitled/services/connectivity_service.dart';
+
+class ConnectivityProvider with ChangeNotifier {
+  final ConnectivityService _connectivityService = ConnectivityService();
+  List<ConnectivityResult> _connectivityResults = [];
+
+  List<ConnectivityResult> get connectivityResults => _connectivityResults;
+
+  ConnectivityProvider() {
+    _initConnectivity();
+    _connectivityService.connectivityStream.listen((results) {
+      _connectivityResults = results;
+      notifyListeners();
+    });
+  }
+
+  Future<void> _initConnectivity() async {
+    try {
+      _connectivityResults = await _connectivityService.checkConnectivity();
+      notifyListeners();
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error: $e');
+      }
+    }
+  }
+}
