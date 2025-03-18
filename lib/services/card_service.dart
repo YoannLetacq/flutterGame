8c4e9bcba2f0aa4cc7d05731652c3ad9717808bd
Yoann <yoann.letacq0@gmail.com>
2025-03-18 20:25:23 +0100

    CREATE card_service.dart (get card list from firestore)

---
+++ lib/services/card_service.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+import 'package:untitled/models/card_model.dart';
+
+
+/// Service pour récupérer les cartes depuis Firestore.
+class CardService {
+  final FirebaseFirestore _firestore;
+
+  CardService({FirebaseFirestore? firestore})
+      : _firestore = firestore ?? FirebaseFirestore.instance;
+
+  /// Récupère la liste des cartes depuis la collection "cards".
+  Future<List<CardModel>> fetchCards() async {
+    try {
+      final snapshot = await _firestore.collection('cards').get();
+      if (kDebugMode) {
+        print('Cartes récupérées: ${snapshot.docs.length}');
+      }
+      return snapshot.docs.map((doc) => CardModel.fromJson(doc.data())).toList();
+    } catch (e, stackTrace) {
+      if (kDebugMode) {
+        print('Erreur lors de la récupération des cartes: $e');
+        print(stackTrace);
+      }
+      rethrow;
+    }
+  }
+}
