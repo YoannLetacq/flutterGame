587eed2791aa8752c88d9acda10f389c34fe0582
Yoann <yoann.letacq0@gmail.com>
2025-03-18 15:23:17 +0100

    UPDATE history_service.dart (implement security service to validate historyData)

--- lib/services/history_service.dart
+++ lib/services/history_service.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
+import 'package:untitled/services/security_service.dart';
 
 class HistoryService {
   final FirebaseFirestore _firestore;
+  final SecurityService _securityService = SecurityService();
 
   HistoryService({FirebaseFirestore? firestore})
       : _firestore = firestore ?? FirebaseFirestore.instance;
@@ -10,6 +12,9 @@ class HistoryService {
   /// Enregistre l'historique d'une partie pour l'utilisateur [userId].
   /// [historyData] doit contenir les informations de la partie (date, résultat, score, exp, eloChange, etc.)
   Future<void> recordGameHistory(String userId, Map<String, dynamic> historyData) async {
+    if (!_securityService.validateHistoryData(historyData)) {
+      throw Exception("Données d'historique invalides");
+    }
     try {
       await _firestore
           .collection('game_history')
