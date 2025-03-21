3a548e8c9063c805eb62af907de21d6aa638b341
Yoann <yoann.letacq0@gmail.com>
2025-03-21 11:51:35 +0100

    UPDATE card_service.dart (ensure card fetching)

--- lib/services/card_service.dart
+++ lib/services/card_service.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
+import 'package:untitled/constants/app_constants.dart';
 import 'package:untitled/models/card_model.dart';
-
 
 /// Service pour récupérer les cartes depuis Firestore.
 class CardService {
@@ -10,10 +10,10 @@ class CardService {
   CardService({FirebaseFirestore? firestore})
       : _firestore = firestore ?? FirebaseFirestore.instance;
 
-  /// Récupère la liste des cartes depuis la collection "cards".
+  /// Récupère la liste des cartes depuis la collection Firestore définie.
   Future<List<CardModel>> fetchCards() async {
     try {
-      final snapshot = await _firestore.collection('cards').get();
+      final snapshot = await _firestore.collection(FirestoreCollections.cards).get();
       if (kDebugMode) {
         print('Cartes récupérées: ${snapshot.docs.length}');
       }
