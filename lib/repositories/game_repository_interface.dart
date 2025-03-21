3a28af11f5e00a5b8cb104fecb73cc04e7ee8f15
Yoann <yoann.letacq0@gmail.com>
2025-03-21 11:43:37 +0100

    UPDATE game_repository_interface.dart (change update method signature)

--- lib/repositories/game_repository_interface.dart
+++ lib/repositories/game_repository_interface.dart
@@ -1,13 +1,14 @@
 abstract class IGameRepository {
-  /// Crée une nouvelle partie dans la base de données.
+  /// Crée une nouvelle partie avec les données fournies.
   Future<void> createGame(Map<String, dynamic> gameData);
 
-  /// Récupère les données d'une partie en fonction de son identifiant.
+  /// Récupère les données d'une partie par son [gameId].
+  /// Retourne un Map des données de la partie ou `null` si non trouvée.
   Future<Map<String, dynamic>?> getGame(String gameId);
 
-  /// Met à jour les données d'une partie.
-  Future<void> updateGame(String gameId, Map<String, dynamic> gameData);
+  /// Met à jour les données partielles de la partie [gameId] avec [updates].
+  Future<void> updateGame(String gameId, Map<String, dynamic> updates);
 
-  /// Supprime une partie à partir de son identifiant.
+  /// Supprime la partie identifiée par [gameId].
   Future<void> deleteGame(String gameId);
 }
