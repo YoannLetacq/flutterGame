f27358bfe046701fc3cedd40a119baa033eebe9f
Yoann <yoann.letacq0@gmail.com>
2025-03-31 11:23:47 +0200

    CREATE navigator_helper.dart (utility function to preload provider before redirection)

---
+++ lib/helpers/navigator_helper.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+import '../models/game_model.dart';
+import '../providers/game_state_provider.dart';
+import '../services/abandon_service.dart';
+import '../services/elo_service.dart';
+import '../services/game_flow_service.dart';
+import '../services/game_progress_service.dart';
+import '../services/response_service.dart';
+import '../services/timer_service.dart';
+import '../ui/game_screen.dart';
+
+/// Fonction utilitaire pour lancer correctement la GameScreen avec son provider
+void navigateToGame(BuildContext context, GameModel game) {
+  Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (_) => ChangeNotifierProvider(
+        create: (_) => GameStateProvider(
+          gameFlowService: GameFlowService(
+            timerService: context.read<TimerService>(),
+            progressService: context.read<GameProgressService>(),
+            abandonService: context.read<AbandonService>(),
+            eloService: context.read<EloService>(),
+            game: game,
+            gameRef: FirebaseDatabase.instance.ref('games/${game.id}'),
+          ),
+          responseService: ResponseService(),
+          timerService: context.read<TimerService>(),
+          gameProgressService: context.read<GameProgressService>(),
+          cards: game.cards,
+        ),
+        child: GameScreen(game: game),
+      ),
+    ),
+  );
+}
