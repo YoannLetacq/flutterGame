ea67c54ee72de0d63d60b8efd753662041654927
Yoann <yoann.letacq0@gmail.com>
2025-03-31 16:51:40 +0200

    UPDATE realtime_db_helper.dart (remove run transaction to use the one provide by firebase)

--- lib/helpers/realtime_db_helper.dart
+++ lib/helpers/realtime_db_helper.dart
@@ -8,29 +8,13 @@ class RealtimeDBHelper {
     databaseURL: 'https://game-76a82-default-rtdb.europe-west1.firebasedatabase.app',
   );
 
-  /// Convert any value to Map if possible
-  static Map<String, dynamic> _normalize(dynamic value) {
-    if (value is Map<String, dynamic>) {
-      return value;
-    } else if (value != null && value.toJson is Function) {
-      return value.toJson() as Map<String, dynamic>;
-    } else {
-      throw ArgumentError('Value must be a Map<String, dynamic> or have a toJson() method');
-    }
-  }
-
   /// set or create data
   static Future<void> setData(String path, dynamic value) async {
     try {
-      final data = _normalize(value);
-      await _db.ref(path).set(data);
-      if (kDebugMode) {
-        print('Data set: $path');
-      }
+      await _db.ref(path).set(value);
+      if (kDebugMode) print('Data set: $path');
     } catch (e) {
-      if (kDebugMode) {
-        print('Error setting data: $e');
-      }
+      if (kDebugMode) print('Error setting data: $e');
       rethrow;
     }
   }
@@ -40,9 +24,7 @@ class RealtimeDBHelper {
     try {
       return await _db.ref(path).get();
     } catch (e) {
-      if (kDebugMode) {
-        print('Error getting data: $e');
-      }
+      if (kDebugMode) print('Error getting data: $e');
       rethrow;
     }
   }
@@ -50,15 +32,10 @@ class RealtimeDBHelper {
   /// update data
   static Future<void> updateData(String path, dynamic updates) async {
     try {
-      final data = _normalize(updates);
-      await _db.ref(path).update(data);
-      if (kDebugMode) {
-        print('Data updated at $path => $data');
-      }
+      await _db.ref(path).update(updates);
+      if (kDebugMode) print('Data updated at $path => $updates');
     } catch (e) {
-      if (kDebugMode) {
-        print('Error updating data: $e');
-      }
+      if (kDebugMode) print('Error updating data: $e');
       rethrow;
     }
   }
@@ -67,50 +44,20 @@ class RealtimeDBHelper {
   static Future<void> removeData(String path) async {
     try {
       await _db.ref(path).remove();
-      if (kDebugMode) {
-        print('Data removed: $path');
-      }
+      if (kDebugMode) print('Data removed: $path');
     } catch (e) {
-      if (kDebugMode) {
-        print('Error removing data: $e');
-      }
+      if (kDebugMode) print('Error removing data: $e');
       rethrow;
     }
   }
 
-  /// run transaction
-  static Future<TransactionResult> runTransaction(String path, TransactionHandler transactionHandler) async {
-    try {
-      return await _db.ref(path).runTransaction(transactionHandler);
-    } catch (e) {
-      if (kDebugMode) {
-        print('Error running transaction: $e');
-      }
-      rethrow;
-    }
+  /// get a DatabaseReference
+  static DatabaseReference ref(String path) {
+    return _db.ref(path);
   }
 
-  /// get a reference
-  static Future<DatabaseReference> ref(String path) async {
-    try {
-      return _db.ref(path);
-    } catch (e) {
-      if (kDebugMode) {
-        print('Error getting reference: $e');
-      }
-      rethrow;
-    }
-  }
-
-  /// push data
-  static Future<DatabaseReference> push(String path) async {
-    try {
-      return _db.ref(path).push();
-    } catch (e) {
-      if (kDebugMode) {
-        print('Error pushing data: $e');
-      }
-      rethrow;
-    }
+  /// push node
+  static DatabaseReference push(String path) {
+    return _db.ref(path).push();
   }
 }
