c502af45597a8f24402e3c408df267fc88d506a4
Yoann <yoann.letacq0@gmail.com>
2025-04-25 18:12:38 +0200

    UPDATE learning_search_bar_widget.dart (pass to statefull widget update the textcontroller widget to follow the search display)

--- lib/ui/widgets/learning_search_bar_widget.dart
+++ lib/ui/widgets/learning_search_bar_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class LearningSearchBar extends StatelessWidget {
+class LearningSearchBar extends StatefulWidget {
   final String query;
   final ValueChanged<String> onSearch;
 
@@ -11,18 +11,40 @@ class LearningSearchBar extends StatelessWidget {
   });
 
   @override
+  State<LearningSearchBar> createState() => _LearningSearchBarState();
+}
+
+class _LearningSearchBarState extends State<LearningSearchBar> {
+  late final TextEditingController _ctrl;
+
+  @override
+  void initState() {
+    super.initState();
+    _ctrl = TextEditingController(text: widget.query);
+  }
+
+  @override
+  void dispose() {
+    _ctrl.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(12),
       child: TextField(
-        onChanged: onSearch,
-        controller: TextEditingController(text: query),
+        controller: _ctrl,
+        onChanged: widget.onSearch,
         decoration: InputDecoration(
           prefixIcon: const Icon(Icons.search),
-          suffixIcon: query.isNotEmpty
+          suffixIcon: _ctrl.text.isNotEmpty
               ? IconButton(
             icon: const Icon(Icons.clear),
-            onPressed: () => onSearch(''),
+            onPressed: () {
+              _ctrl.clear();
+              widget.onSearch('');
+            },
           )
               : null,
           hintText: 'Rechercher une notion…',
