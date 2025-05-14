d538bd587041a5810359bdee49a461623f781cd4
Yoann <yoann.letacq0@gmail.com>
2025-05-14 09:59:11 +0200

    UPDATE matchmaking_service.dart (comment change) UPDATE game_screen.dart ( show banner remove modify disconnect_notif_widget.dart add a quit button to widgete)

--- lib/ui/widgets/disconnect_notif_widget.dart
+++ lib/ui/widgets/disconnect_notif_widget.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-class DisconnectNotifWidget extends StatelessWidget {
+class DisconnectNotifWidget extends StatefulWidget {
   final IconData icon;
-  final String   message;
+  final String message;
+
   const DisconnectNotifWidget._(this.icon, this.message, {super.key});
 
   factory DisconnectNotifWidget.disconnect() => const DisconnectNotifWidget._(
@@ -17,13 +18,38 @@ class DisconnectNotifWidget extends StatelessWidget {
   );
 
   @override
+  State<DisconnectNotifWidget> createState() => _DisconnectNotifWidgetState();
+}
+
+class _DisconnectNotifWidgetState extends State<DisconnectNotifWidget> {
+  bool _isVisible = true;
+
+  @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Icon(icon, size: 24),
-        const SizedBox(width: 8),
-        Expanded(child: Text(message)),
-      ],
+    if (!_isVisible) return const SizedBox.shrink();
+
+    return Container(
+      padding: const EdgeInsets.all(12),
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      decoration: BoxDecoration(
+        color: Colors.grey.shade200,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Row(
+        children: [
+          Icon(widget.icon, size: 24),
+          const SizedBox(width: 8),
+          Expanded(child: Text(widget.message)),
+          IconButton(
+            icon: const Icon(Icons.close),
+            onPressed: () {
+              setState(() {
+                _isVisible = false;
+              });
+            },
+          )
+        ],
+      ),
     );
   }
 }
